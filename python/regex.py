3ab1858aeef20f9a9e76262bd428965540c99e4b
zinkjm <zinkjm@mail.uc.edu>
2023-03-07 18:21:57 -0500

    python in class

---
+++ python/regex.py
@@ -0,0 +1,5 @@
+import requests, re
+
+data = "Hello. My email is reedws@ucmail.uc.edu. Please contact me!"
+p = re.compile('[A-Za-z0-9_%.-]+@[A-z0-9_%.-]+\.[A-z0-9]{2,}')
+print(p.search(data).group())
---
+++ python/webscraping.py
@@ -0,0 +1,8 @@
+from bs4 import BeautifulSoup
+import requests, re
+
+r = requests.get('https://analytics.usa.gov').content
+soup = BeautifulSoup(r, 'html.parser')
+print(type(soup))
+print(soup.prettify()[:100])
+for link in soup.find_all('a'): print(link.get('href'))
